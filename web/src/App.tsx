a8c804147628457dd608c2bea79b15ef47833176
Igor Tuag <igortuag@gmail.com>
2020-06-03 20:32:18 -0300

    Construcao do HTML da primeira pagina

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,23 +2,12 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// JSX: Sintaxe de XML dentro do JavaScript
-
-import Header from './Header';
+import Home from './pages/Home/index'
 
 function App() {
-  const [counter, setCounter] = useState(0); // [valor do estado, função para atualizar o valor do estado]
-
-  function handleButtonClick() {
-      setCounter(counter + 1)
-  }
-
   return (
     <div>
-      <Header title={`Contador: ${counter}`}/>
-
-      <h1>{counter}</h1>
-      <button type='button' onClick={handleButtonClick}>Aumentar</button>
+      <Home />
     </div>
   );
 }
