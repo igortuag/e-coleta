f5d18027bff4dbeb55da6896906a160a86a919ad
Igor Tuag <igortuag@gmail.com>
2020-06-03 19:28:36 -0300

    Limpando estrutura de arquivos nao utilizados

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello World!</h1>
-      </header>
-    </div>
   );
 }
 
