f899d8e06da942fd66d6707b115bc91af9a980fa
Igor Tuag <igortuag@gmail.com>
2020-06-03 19:36:06 -0300

    Conceito JSX

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// JSX: Sintaxe de XML dentro do JavaScript
+
 function App() {
   return (
-          <h1>Hello World!</h1>
+    <h1>Hello World!</h1>
   );
 }
 
