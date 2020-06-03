a53a6da205ec135af5c13bafa6746ab6092961e2
Igor Tuag <igortuag@gmail.com>
2020-06-02 21:09:06 -0300

    Secound Day - Backend

---
+++ server/src/routes.ts
@@ -0,0 +1,18 @@
+import express, { response } from 'express';
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
+// index, show, create, update, delete
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index)
+
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
+
+export default routes;
