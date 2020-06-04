0e990950b54ea41481ec39a3b7eb947574fac1d8
Igor Tuag <igortuag@gmail.com>
2020-06-03 22:55:57 -0300

    Mensagem de sucesso

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -9,8 +9,6 @@ import api from '../../services/api';
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
-
-// Sempre que criamos um estado para um array ou objeto precisa informar manualmente o tipo da variavel que será armazenada ali dentro
 
 interface Item {
     id: number;
@@ -43,6 +41,8 @@ const CreatePoint = () => {
     const [selectedCities, setSelectedCities] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+    const history = useHistory();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
@@ -117,8 +117,31 @@ const CreatePoint = () => {
         }
     }
 
-    function handleSubmit() {
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
 
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCities;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+
+        await api.post('points', data)
+
+        alert('Ponto de coleta criado!')
+
+        history.push('/');
     }
 
     return (
